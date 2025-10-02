219f803cdd72e3271dcbadfbd48d1e3f599fe318
ASiaka <alghenriyb@gmail.com>
2025-10-02 23:21:14 +0200

    ajout var_dumper & altorouteur & gitignore

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -26,6 +26,8 @@ class MainController {
     }
 
     private function show($page, $articles = []) {
+        $baseUri = $_SERVER['BASE_URI'];
+        
         require_once __DIR__ . "/../views/includes/header.php";
         require_once __DIR__ . "/../views/" . $page . ".php";
         require_once __DIR__ . "/../views/includes/footer.php";
--- public/index.php
+++ public/index.php
@@ -3,6 +3,9 @@ require '../app/views/includes/config.php';
 $stmt = $pdo->query("SELECT * FROM articles ORDER BY date_creation DESC");
 $articles = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+// Chargement des dépendances via autoload.php de composer
+require __DIR__ . '/../vendor/autoload.php';
+
 require __DIR__ . "/../app/controllers/MainController.php";
 
 $page = [
@@ -11,6 +14,16 @@ $page = [
     '/contact' => 'contact',
     '/article' => 'article',
 ];
+
+$router = new AltoRouter();
+$router->setBasePath($_SERVER['BASE_URI']);
+$router->map( 'GET', '/', ['/' => 'accueil'], 'home' );
+$match = $router->match();
+
+// var_dump() avec var_dumper
+// dump($page);
+// var_dump() & die avec var_dumper
+// dd($page);
 
 if(isset($_GET['_url'])) {
     // Page par défaut
